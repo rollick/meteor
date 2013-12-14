aa88e8786441451e43b53f731d8baf7837bfe3de
Slava Kim <slava@meteor.com>
2013-12-13 16:40:12 -0800

    Pull all imports with css-parse

--- packages/minifiers/minifiers.js
+++ packages/minifiers/minifiers.js
@@ -1,23 +1,15 @@
 var CleanCss = Npm.require('clean-css');
 
 CleanCSSProcess = function (source, options) {
+  // clean-css doesn't work well with some kinds of imports.
   options = _.extend({ processImport: false }, options);
   var instance = new CleanCss(options);
-  // after concatenation some @import's might be left in the middle of CSS file
-  // but they required to be in the beginning.
-  source = CSSPullImports(source);
   return instance.minify(source);
 };
 
+CssParse = Npm.require('css-parse');
+CssStringify = Npm.require('css-stringify');
+
 UglifyJSMinify = Npm.require('uglify-js').minify;
 
-var CSSPullImports = function (source) {
-  var importRegExp = /^\s*@import\s*[^;]*;\s*/gm;
-  var imports = source.match(importRegExp) || [];
-  var newSource = source.replace(importRegExp, '');
-
-  newSource = imports.join('') + newSource;
-
-  return newSource;
-}
 
